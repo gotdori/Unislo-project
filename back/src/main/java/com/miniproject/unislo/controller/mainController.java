b7f15a4e00524f8fdeb000b1dbd24fec3ae7d12d
HakyungYoun <dbsgkrud5379@naver.com>
2023-06-01 16:43:47 +0900

    init : proxy

--- back/src/main/java/com/miniproject/unislo/controller/mainController.java
+++ back/src/main/java/com/miniproject/unislo/controller/mainController.java
@@ -1,13 +1,13 @@
 package com.miniproject.unislo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class mainController {
     
     @GetMapping(value = "/")
     public String main(){
-        return "index";
+        return "gimochi";
     }
 }
